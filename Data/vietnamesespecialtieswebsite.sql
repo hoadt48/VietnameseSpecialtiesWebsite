7b111dbe0ec4e58402795d9a0f02d8019a0944d2
hoadt48 <hoadt48@gmail.com>
2019-05-20 02:22:57 +0700

    [VSW][APP] Create user, admin, product table #4
    [ISSUE] VSW #4
    [DES] Create user, admin, product table #4

--- Data/vietnamesespecialtieswebsite.sql
+++ Data/vietnamesespecialtieswebsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2019 at 12:27 AM
+-- Generation Time: May 19, 2019 at 09:20 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -21,6 +21,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `vietnamesespecialtieswebsite`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` int(16) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '1',
+  `level` tinyint(4) DEFAULT '1',
+  `avatar` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,12 +64,66 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `slug`, `images`, `banner`, `home`, `status`, `created_at`, `updated_at`) VALUES
-(0, 'Bưởi Diễn', 'buoi-dien', '', '', 0, 1, '2019-05-01 22:27:00', '2019-05-01 22:27:00'),
-(1, 'Bưởi Đào', 'buoi-dao', '', '', 0, 1, '2019-05-01 22:27:13', '2019-05-01 22:27:13');
+(2, 'Hồng bì', NULL, '', '', 0, 1, '2019-05-07 02:30:30', '2019-05-07 02:30:30'),
+(3, 'Ngọn su su', NULL, '', '', 0, 1, '2019-05-07 04:23:55', '2019-05-07 04:23:55'),
+(4, 'Đào mỏ quạ', NULL, '', '', 0, 1, '2019-05-07 04:24:58', '2019-05-07 04:24:58'),
+(5, 'Trái Mơ', NULL, '', '', 0, 1, '2019-05-07 04:56:00', '2019-05-07 04:56:00'),
+(6, 'Trái Cam', NULL, '', '', 0, 1, '2019-05-07 04:56:09', '2019-05-07 04:56:09'),
+(7, 'Trái Mít', NULL, '', '', 0, 1, '2019-05-07 04:56:24', '2019-05-07 04:56:24'),
+(8, 'Trái Đu Đủ', NULL, '', '', 0, 1, '2019-05-07 04:56:45', '2019-05-07 04:56:45'),
+(12, 'Bưởi Đoan Hùng', 'buoi-doan-hung', '', '', 0, 1, '2019-05-19 17:09:53', '2019-05-19 17:09:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `slug` int(100) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '0',
+  `thunbar` varchar(100) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `content` text,
+  `head` int(11) DEFAULT '0',
+  `view` int(11) DEFAULT '0',
+  `hot` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` char(16) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `avatar` int(100) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '1',
+  `token` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `category`
@@ -59,14 +132,44 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
