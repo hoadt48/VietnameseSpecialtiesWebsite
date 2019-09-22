41b498eeea6a0808dc14934f1c52b3a04a3cd428
hoadt48 <hoadt48@gmail.com>
2019-09-22 17:27:17 +0700

    Add database

--- Data/vietnamesespecialtieswebsite.sql
+++ Data/vietnamesespecialtieswebsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2019 at 09:20 PM
+-- Generation Time: Sep 22, 2019 at 12:25 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -38,8 +38,19 @@ CREATE TABLE `admin` (
   `level` tinyint(4) DEFAULT '1',
   `avatar` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `password` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `address`, `email`, `phone`, `status`, `level`, `avatar`, `created_at`, `updated_at`, `password`) VALUES
+(4, 'fdsf', NULL, '33', 333, 1, 0, NULL, '2019-06-10 16:56:03', '2019-06-10 16:56:03', '222'),
+(5, 'fdsf', NULL, 'fsdf', 0, 1, 0, NULL, '2019-06-10 17:13:05', '2019-06-10 17:13:05', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(6, 'fdsf', NULL, 'fdsfsdf', 333, 1, 0, NULL, '2019-06-10 17:56:20', '2019-06-10 17:56:20', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(7, 'aaa2', NULL, 'sss', 222, 1, 0, NULL, '2019-06-10 17:57:11', '2019-06-10 17:59:33', '5f4dcc3b5aa765d61d8327deb882cf99');
 
 -- --------------------------------------------------------
 
@@ -64,14 +75,24 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `slug`, `images`, `banner`, `home`, `status`, `created_at`, `updated_at`) VALUES
-(2, 'Hồng bì', NULL, '', '', 0, 1, '2019-05-07 02:30:30', '2019-05-07 02:30:30'),
-(3, 'Ngọn su su', NULL, '', '', 0, 1, '2019-05-07 04:23:55', '2019-05-07 04:23:55'),
-(4, 'Đào mỏ quạ', NULL, '', '', 0, 1, '2019-05-07 04:24:58', '2019-05-07 04:24:58'),
-(5, 'Trái Mơ', NULL, '', '', 0, 1, '2019-05-07 04:56:00', '2019-05-07 04:56:00'),
-(6, 'Trái Cam', NULL, '', '', 0, 1, '2019-05-07 04:56:09', '2019-05-07 04:56:09'),
-(7, 'Trái Mít', NULL, '', '', 0, 1, '2019-05-07 04:56:24', '2019-05-07 04:56:24'),
-(8, 'Trái Đu Đủ', NULL, '', '', 0, 1, '2019-05-07 04:56:45', '2019-05-07 04:56:45'),
-(12, 'Bưởi Đoan Hùng', 'buoi-doan-hung', '', '', 0, 1, '2019-05-19 17:09:53', '2019-05-19 17:09:53');
+(0, 'Hồng bì vàng', 'hong-bi-vang', '', '', 1, 1, '2019-06-11 04:46:47', '2019-06-11 04:46:47'),
+(2, 'Trái Mơ', NULL, '', '', 1, 1, '2019-06-11 04:47:29', '2019-06-11 04:47:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `transaction_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` tinyint(4) NOT NULL DEFAULT '0',
+  `price` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -82,7 +103,7 @@ INSERT INTO `category` (`id`, `name`, `slug`, `images`, `banner`, `home`, `statu
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `slug` int(100) DEFAULT NULL,
+  `slug` varchar(100) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `status` tinyint(4) DEFAULT '0',
   `thunbar` varchar(100) DEFAULT NULL,
@@ -92,7 +113,33 @@ CREATE TABLE `product` (
   `view` int(11) DEFAULT '0',
   `hot` tinyint(4) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `number` int(11) NOT NULL DEFAULT '0',
+  `sale` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `slug`, `price`, `status`, `thunbar`, `category_id`, `content`, `head`, `view`, `hot`, `created_at`, `updated_at`, `number`, `sale`) VALUES
+(5, 'SS', 'ss', 222, 0, 'dac-san-8.jpg', 0, 'fdsfsdf', 0, 0, 0, '2019-06-02 20:13:38', '2019-06-02 20:13:38', 22, 2),
+(6, 'fdsfds', 'fdsfds', 222, 0, 'dac-san-12.jpg', 0, 'fdsfsdf', 0, 0, 0, '2019-06-11 05:17:37', '2019-06-11 05:17:37', 222, 3),
+(7, 'fdsfdsf', 'fdsfdsf', 2233, 0, 'dac-san-8.jpg', 2, 'dsrfsdf', 0, 0, 0, '2019-06-11 05:17:57', '2019-06-11 05:17:57', 3232, 2323);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `users_id` int(11) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -132,9 +179,21 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -151,18 +210,30 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
